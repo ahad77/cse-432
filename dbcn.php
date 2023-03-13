8bb847266de021f7319637b00f6e1c050eadff74
Ahadul Islam Sakib <ahad.i.sakib@gmail.com>
2023-03-13 14:38:21 +0600

    commit 2

---
+++ dbcn.php
@@ -0,0 +1,10 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "student_req";
+$con= mysqli_connect($hostname,$username,$password,$dbname);
+
+
+
+?>
